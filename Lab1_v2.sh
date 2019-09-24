6526e5a1d1fe32ee510d84e127859ca0584a8a9a
VladimirWayOne <55317956+VladimirWayOne@users.noreply.github.com>
2019-09-24 22:57:33 +0300

    Add files via upload

---
+++ Lab1_v2.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+rm -f ./result.csv
+printf "Путь\tИмя\tРасширение\tРазмер\tДата Изменения\tПродолжительность(HH:MM:SS)\tРазмер изображения\n" >> result.csv
+RESULT=$(pwd)
+
+function extension() {
+	echo $(basename "${FILE}") | grep -Eo '\.[^.]+$'
+}
+
+function FileSize() {
+	b=$(wc -c < "${FILE}")
+	let "c = "$b" / 1024"
+	echo "$c"
+}
+
+function Date() {
+	d=$(date -r "${FILE}")
+	echo "$d"
+}
+
+function fileduration() {
+	dur=$(ffprobe -i "$FILE" -sexagesimal -show_entries format=duration -v quiet -of csv="p=0")
+	echo ${dur%.*}
+}
+
+function ImSize() {
+	IMAGE=$(identify -format "%w\*%h" "$FILE")
+	echo $IMAGE
+}
+
+function fileinfo() {
+	filelist=($(ls))  #массив, состоящий из файлов в текущей директории
+	for LIST in ${filelist[*]} 
+	do
+	printf "$LIST\n"
+	done
+	for FILE in $@
+	do
+		#printf "$(basename ${FILE})\n"
+		if [[ -d "$FILE" ]]
+		then
+			next_dir=$(ls "$FILE")
+			cd "$FILE"
+			#echo "$FILE"
+			fileinfo "$next_dir"
+			cd ..
+		fi
+
+		if [[ -f "${FILE}" ]]
+		then
+			NAME=$(basename "${FILE}")	#Имя файла
+			EXT=$(extension)		#Расширение файла
+			SIZE=$(FileSize)		#Размер файла
+			MOD_DATE=$(Date)		#Дата последней модификации
+			DURATION="-"			#Продолжительность аудио/видео
+			IMAGESIZE="-"			#Размер изображения
+
+			if file "$FILE" | grep -qE 'bitmap|image'	#Если файл - изображение, узнаем его размер
+			then
+				IMAGESIZE=$(ImSize)
+			elif file -ib "$FILE" | grep -qE 'video|audio|octet-stream'	# Если видео/аудио -- его продолжительность
+			then
+				DURATION=$(fileduration)	
+			fi
+			PREVIOUS=$(pwd)	#Директория, в которой сейчас находимся
+			cd "${RESULT}"	#Переход в директорию с файлом результата
+			#printf "$PREVIOUS\t$NAME\t$EXT\t$SIZE\t$MOD_DATE\t$DURATION\t$IMAGESIZE\n"
+			printf "$PREVIOUS\t$NAME\t$EXT\t$SIZE KB\t$MOD_DATE\t$DURATION\t$IMAGESIZE\n" >> result.csv
+			cd "${PREVIOUS}"
+		fi
+	done
+}
+
+START=$(pwd)
+#cd "$1"
+fileinfo "$START"
+
+
