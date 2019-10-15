2a4d3f51997f2c9398f917e1dd7d647a844c1044
VladimirWayOne <55317956+VladimirWayOne@users.noreply.github.com>
2019-10-15 21:05:34 +0300

    Update Lab2_Rekin.sh

--- Lab2_Rekin.sh
+++ Lab2_Rekin.sh
@@ -1,19 +1,6 @@
-#!/bin/bash
-function save() {
-	cd ~
-	echo 'Сохранить проект в...'
-	read way
-	if ! [ -d "$way" ] 
-	then
-		mkdir "$way"
-	fi
-echo "$way"
-}
-save
-git clone https://github.com/Bitmessage/PyBitmessage.git "$way"
-cd "$way"
-python checkdeps.py
-sudo apt-get install python openssl libssl-dev git python-msgpack python-qt4
-#sudo apt install -f
-git clone https://github.com/Bitmessage/PyBitmessage $HOME/PyBitmessage
-~/PyBitmessage/src/bitmessagemain.py
+#!bin/bash
+git clone https://github.com/Bitmessage/PyBitmessage $HOME/PyBitmessage #загружаем репозиторий
+sudo apt-get install python openssl libssl-dev git python-msgpack python-qt4 #resolve dependencies
+cd $HOME/PyBitmessage
+git pull #upgrade
+~/PyBitmessage/src/bitmessagemain.py #run
