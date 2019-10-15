167a3fc2d207543797e91b50c233a0cad0a2345b
VladimirWayOne <55317956+VladimirWayOne@users.noreply.github.com>
2019-10-15 21:06:15 +0300

    Update Lab2_Rekin.sh

--- Lab2_Rekin.sh
+++ Lab2_Rekin.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#! /bin/bash
 git clone https://github.com/Bitmessage/PyBitmessage $HOME/PyBitmessage #загружаем репозиторий
 sudo apt-get install python openssl libssl-dev git python-msgpack python-qt4 #resolve dependencies
 cd $HOME/PyBitmessage
