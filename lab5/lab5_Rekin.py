3923c8626743c37a9d7f7e37a2635612bb4ad98f
VladimirWayOne <55317956+VladimirWayOne@users.noreply.github.com>
2019-12-03 18:58:11 +0300

    Update lab5_Rekin.py

--- lab5/lab5_Rekin.py
+++ lab5/lab5_Rekin.py
@@ -3,8 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def Plotting(x,y, name, Color,Ylabel):
-    Fig = plt.figure(figsize=(35, 15))
+def transactionsFee(block):
+    feesSum = 0
+    for transaction in block.transactions:
+        feesSum += web3.eth.getTransactionReceipt(transaction).gasUsed * web3.eth.getTransaction(transaction).gasPrice * 1e-18
+    return feesSum
+
+
+def Plotting(x, y, name, Color, Ylabel):
+    Fig = plt.figure(figsize=(28, 10))
     ax = Fig.add_subplot()
     ax.set(title=name, xlabel="Block's number", ylabel=Ylabel)
     ax.tick_params(axis='x', labelrotation=90, pad=10)
@@ -17,33 +24,30 @@ def ConvertGweiToETH(gwei):
     return gwei * 1e-8
 
 
-def BlockReward(block):
-    return 2 + ConvertGweiToETH(block.gasUsed) + 2*len(block.uncles)/32
 
+fb, lb = 8961400 - 100 * (13 - 1), 8961400 - 100 * (13 - 2)  # first block, last block
 
-fb, lb = 8961400 - 100 * (13 - 1), 8961400 - 100 * (13 - 2)    #first block, last block
-
-ComissionPercentage = []
-Comission_block = []        # commission for each block
-Reward_block = []           # reward for each block
-web3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/22ccc929d30a47be975b7fee7c05fa3e'))     # Connection via Infura
+CommissionPercentage = []
+Commission_block = []  # commission for each block
+Reward_block = []  # reward for each block
+web3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/22ccc929d30a47be975b7fee7c05fa3e'))  # Connection via Infura
 NumberOfBlock = []
 for blockNumber in range(fb, lb):
+    block = web3.eth.getBlock(blockNumber)  # Current block
+    fee = transactionsFee(block)
     NumberOfBlock.append(blockNumber)
-    block = web3.eth.getBlock(blockNumber)      # Current block
-    Reward_block.append(BlockReward(block))
-    Comission_block.append(ConvertGweiToETH(block.gasLimit))
-    ComissionPercentage.append(Comission_block[len(Comission_block)-1]/Reward_block[len(Reward_block)-1]*100)
+    Reward_block.append(2 + fee + 2*len(block.uncles)/32)
+    Commission_block.append(fee)
+    CommissionPercentage.append(Commission_block[len(Commission_block) - 1] / Reward_block[len(Reward_block) - 1] * 100)
 
+MX = np.mean(Commission_block)  # мат ожидание
+DX = np.var(Commission_block)  # дисперсия
+Me = np.median(Commission_block)  # медиана
+Range = np.ptp(Commission_block)  # размах
+Sigma = np.sqrt(DX)  # среднеквадрат. отклонение
 
-MX = np.mean(Comission_block)   # мат ожидание
-DX = np.var(Comission_block)    # дисперсия
-Me = np.median(Comission_block) # медиана
-Range = np.ptp(Comission_block) # размах
-Sigma = np.sqrt(DX)             # среднеквадрат. отклонение
-
-Plotting(NumberOfBlock, ComissionPercentage, 'Percentage commission for each block', 'red', 'Комиссия, %')
-Plotting(NumberOfBlock, Comission_block, 'Commission for each block', 'm', 'Комиссия, ETH')
+Plotting(NumberOfBlock, CommissionPercentage, 'Percentage commission for each block', 'red', 'Комиссия, %')
+Plotting(NumberOfBlock, Commission_block, 'Commission for each block', 'm', 'Комиссия, ETH')
 Plotting(NumberOfBlock, Reward_block, 'Reward for each block', 'orange', 'Награда, ETH')
 
 print('MX= ', MX, '\nDX= ', DX, '\nMe= ', Me, '\nРазмах= ', Range, '\nδ= ', Sigma)
