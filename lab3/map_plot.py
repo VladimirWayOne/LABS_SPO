a6615ce90f68a432fab26e1bc137f1406b04446f
Vladimir Rekin <55317956+VladimirWayOne@users.noreply.github.com>
2019-12-12 23:21:49 +0300

    Delete map_plot.py

--- lab3/map_plot.py
+++
@@ -1,7 +0,0 @@
-from getData import get_data
-from Map import showMaps
-
-URL = 'http://www.st-petersburg.vybory.izbirkom.ru/region/region/st-petersburg?action=show&tvd=27820001217417&vrn=27820001217413&region=78&global=&sub_region=78&prver=0&pronetvd=null&vibid=27820001217443&type=222'
-data = get_data(URL)
-
-showMaps(data)
